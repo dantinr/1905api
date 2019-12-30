663ce1e8c733948a21d1a7adb1772e01661d234f
dante <dantinr@gmail.com>
2019-12-31 06:08:09 +0800

    debug

--- app/Http/Controllers/Alipay/PayController.php
+++ app/Http/Controllers/Alipay/PayController.php
@@ -10,39 +10,37 @@ class PayController extends Controller
 
 
     /**
-     * 支付宝异步通知
+     * 支付宝异步通知  https://docs.open.alipay.com/200/106120
+     * 普通公钥验签
      */
     public function notify()
     {
-
-        $data1 = file_get_contents("php://input");
+        //$data1 = file_get_contents("php://input");
         $data2 = json_encode($_POST);
-        $log1 = date('Y-m-d H:i:s') . ' >>> ' .$data1 . "\n";
+        //$log1 = date('Y-m-d H:i:s') . ' >>> ' .$data1 . "\n";
         $log2 = date('Y-m-d H:i:s') . ' >>> ' .$data2 . "\n";
-        file_put_contents('alipay.log',$log1,FILE_APPEND);
-        file_put_contents('alipay.log',$log2,FILE_APPEND);
-
+        //file_put_contents('alipay.log',$log1,FILE_APPEND);
+        file_put_contents('logs/alipay.log',$log2,FILE_APPEND);
         $data = $_POST;
-        $sign = $data['sign'];
+        $sign = base64_decode($data['sign']);
         unset($data['sign_type']);
+        unset($data['sign']);
 
+        //echo '<pre>';print_r($data);echo '</pre>';
+        $d = [];
         foreach($data as $k=>$v){
-            $data[$k] = urldecode($v);
+            $d[$k] = urldecode($v);
         }
+        //echo '<pre>';print_r($d);echo '</pre>';die;
 
-        ksort($data);
+        ksort($d);
         $str = "";
-        foreach($data as $k=>$v){
+        foreach($d as $k=>$v){
             $str .= $k . '=' . $v . '&';
         }
 
         //带签名字符串
         $str = rtrim($str,'&');
-
-        $sign = base64_decode($_POST['sign']);      // base64解码
-
-        //$this->verify($str,$sign);      //验签
-
         //读取公钥文件
         $pubKey = file_get_contents(storage_path('keys/ali_pub'));
         //转换为openssl格式密钥
@@ -52,10 +50,10 @@ class PayController extends Controller
         openssl_free_key($res);
         if($result){
             $log = date('Y-m-d H:i:s') . ' >>> 验签通过 1' . "\n\n";
-            file_put_contents("alipay.log",$log,FILE_APPEND);
+            file_put_contents("logs/alipay.log",$log,FILE_APPEND);
         }else{
             $log = date('Y-m-d H:i:s') . ' >>> 验签失败 0' . "\n\n";
-            file_put_contents("alipay.log",$log,FILE_APPEND);
+            file_put_contents("logs/alipay.log",$log,FILE_APPEND);
         }
     }
 
