24357275ade98c0589181d6e613d232e70bad8ce
dante <dantinr@gmail.com>
2020-02-05 14:12:22 +0800

    api 鉴权 中间件

--- app/Http/Middleware/checkToken.php
+++ app/Http/Middleware/checkToken.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use GuzzleHttp\Client;
 
 class checkToken
 {
@@ -16,6 +17,26 @@ class checkToken
     public function handle($request, Closure $next)
     {
         //鉴权 ，验证 token是否有效
+        $uid = $_SERVER['HTTP_UID'];
+        $token = $_SERVER['HTTP_TOKEN'];
+
+        //请求passport 实现鉴权
+        $client = new Client();
+        $response = $client->request('POST', 'http://passport.1905.com/api/auth', [
+            'form_params' => [
+                'uid' => $uid,
+                'token' => $token,
+            ]
+        ]);
+
+        //接收请求响应
+        $response_data = $response->getBody();
+        $arr = json_decode($response_data,true);
+
+        //判断鉴权是否成功
+        if($arr['errno']>0){        //鉴权失败
+            echo "鉴权失败";die;
+        }
 
         return $next($request);
     }
